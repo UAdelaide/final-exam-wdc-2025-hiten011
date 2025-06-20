9f001964ca60eee5a778aca2334d80a78a6fd74f
hiten <hitengupta12345@gmail.com>
2025-06-20 02:14:54 +0000

    [automatic commit] 2025-06-20 02:14:54 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 router.get('/api/dogs', async function(req, res, next) {
     try {
         const [rows] = await db.query(
-            'SELECT * FROM Dogs'
+            'SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id'
         );
 
         res.send(rows);
