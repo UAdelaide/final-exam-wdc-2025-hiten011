22e4dc36a30304cb737b829e93d68504b582a889
hiten <hitengupta12345@gmail.com>
2025-06-20 01:26:57 +0000

    [automatic commit] 2025-06-20 01:26:57 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -66,6 +66,6 @@ INSERT INTO Dogs (owner_id, name, size)
 VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-((SELECT user_id FROM Users WHERE username = 'eveowner'), 'Charlie', 'large'),
+((SELECT user_id FROM Users WHERE username = 'hiten'), '', 'large'),
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Daisy', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Rocky', 'medium');
