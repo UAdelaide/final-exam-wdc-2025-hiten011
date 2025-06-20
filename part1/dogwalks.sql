02537fbcedfaec1c2f94e0b2319654df5a809578
hiten <hitengupta12345@gmail.com>
2025-06-20 01:22:55 +0000

    [automatic commit] 2025-06-20 01:22:55 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -53,3 +53,11 @@ CREATE TABLE WalkRatings (
     FOREIGN KEY (owner_id) REFERENCES Users(user_id),
     CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
 );
+
+INSERT INTO Users (username, email, password_hash, role)
+VALUES
+('alice123', 'alice@example.com', 'hashed123', 'owner'),
+('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('hiten', 'hiten@example.com', 'hashe', 'walker'),
+('eveowner', 'eve@example.com', 'hashed999', 'owner');
