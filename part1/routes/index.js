e79e349f2657a4e87f12e565345979a741cef373
hiten <hitengupta12345@gmail.com>
2025-06-20 02:01:30 +0000

    [automatic commit] 2025-06-20 02:01:30 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/dogs', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  .
 });
 
 router.get('/api/walkrequests/open', function(req, res, next) {
