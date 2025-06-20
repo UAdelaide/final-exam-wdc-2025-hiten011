d1a9f46897812481341d3864707fbbb77e4f7c5a
hiten <hitengupta12345@gmail.com>
2025-06-20 01:28:17 +0000

    [automatic commit] 2025-06-20 01:28:17 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -67,5 +67,5 @@ VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'hiten'), 'josheen', 'small'),
-((SELECT user_id FROM Users WHERE username = 'josheen'), 'Daisy', 'small'),
-((SELECT user_id FROM Users WHERE username = 'carol123'), 'Rocky', 'medium');
+((SELECT user_id FROM Users WHERE username = 'josheen'), 'joe', 'large'),
+((SELECT user_id FROM Users WHERE username = 'joe'), 'liri', 'medium');
