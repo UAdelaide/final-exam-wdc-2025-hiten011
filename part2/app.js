6b4ef3a19a2665ca03b8a19d47efb8cd39dfa115
hiten <hitengupta12345@gmail.com>
2025-06-20 08:35:04 +0000

    [automatic commit] 2025-06-20 08:35:04 UTC

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+var cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
