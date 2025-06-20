ef28322fe6cbbbcc8e2762a698837118599c47b0
hiten <hitengupta12345@gmail.com>
2025-06-20 08:25:37 +0000

    [automatic commit] 2025-06-20 08:25:37 UTC

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/owner-dashboard', async (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/settings.html'));
+  res.sendFile(path.join(, '../../frontend/settings.html'));
 });
 
 router.get('/walker-dashboard', async (req, res) => {
