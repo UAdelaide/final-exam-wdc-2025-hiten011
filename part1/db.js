2462f8bc018864e2cc82257221aae7e94d5c83a2
hiten <hitengupta12345@gmail.com>
2025-06-20 01:56:08 +0000

    [automatic commit] 2025-06-20 01:56:08 UTC

--- part1/db.js
+++ part1/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: ,
-  user: DB_USER,
-  password: DB_PASS,
+  host: localhost,
+  user: root,
+  password: ,
   database: DB_NAME,
 });
 
