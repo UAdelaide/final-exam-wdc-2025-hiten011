8ab93382e6d1ae78ed443ad05858fc681acb4887
hiten <hitengupta12345@gmail.com>
2025-06-20 02:32:24 +0000

    [automatic commit] 2025-06-20 02:32:24 UTC

--- part1/app.js
+++ part1/app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mysql = require('mysql2/promise');
+
+// Inserting database
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
