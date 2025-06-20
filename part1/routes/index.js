5d8d4455a080ee7517548dc5702e46810a42199d
hiten <hitengupta12345@gmail.com>
2025-06-20 02:03:10 +0000

    [automatic commit] 2025-06-20 02:03:10 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -8,7 +8,36 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/dogs', function(req, res, next) {
-  
+   // checking if user Vector exists
+    const userId = req.user.id;
+    var isUserVector = false;
+    try {
+
+        const [rows] = await db.query(
+            'SELECT user_vector FROM USERSETTINGS WHERE user_id = ?',
+            [userId]
+        );
+
+        if (!rows[0].user_vector) {
+            isUserVector = false;
+        } else {
+            isUserVector = true;
+        }
+    } catch (err) {
+        isUserVector = false;
+    }
+
+    if (!isUserVector) {
+        // if first time user
+        res.sendFile(path.join(__dirname, '../../frontend/personalise.html'));
+    } else {
+        res.sendFile(path.join(__dirname, '../../frontend/personalise-swipe.html'));
+    }
+
+    req.session.visited = true;
+    // res.send('works');
+
+    // console.log(req.session.id);
 });
 
 router.get('/api/walkrequests/open', function(req, res, next) {
