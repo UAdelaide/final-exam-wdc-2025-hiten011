5a4ec7dd3a77f6db8bae54339ba020befba06651
hiten <hitengupta12345@gmail.com>
2025-06-20 01:23:15 +0000

    [automatic commit] 2025-06-20 01:23:15 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -59,5 +59,5 @@ VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('hiten', 'hiten@example.com', 'hashe', 'walker'),
-('eveowner', 'eve@example.com', 'hashed999', 'owner');
+('hiten', 'hiten@example.com', 'hashed1234', 'walker'),
+('josheen', 'josheen@example.com', 'hashed1234', 'owner');
