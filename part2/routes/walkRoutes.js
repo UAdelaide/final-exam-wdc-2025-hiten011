d528ecd60e26099bacc6a227b73146c87ee97ea1
hiten <hitengupta12345@gmail.com>
2025-06-21 02:52:54 +0000

    [automatic commit] 2025-06-21 02:52:54 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -62,7 +62,7 @@ router.post('/:id/apply', async (req, res) => {
 router.get('/dogs', async function (req, res, next) {
     try {
         const [rows] = await db.query(
-            `SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id`
+            `SELECT d.name AS dog_name, d.size, d AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id`
         );
 
         res.send(rows);
